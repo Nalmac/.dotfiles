0af7778af5fa617cc38d6c05df4cca3799569749
= <=>
2025-12-18 09:40:36 +0100

    install.sh complété

--- install.sh
+++ install.sh
@@ -8,3 +8,6 @@ ln -s ~/.dotfiles/wofi ~/.config/wofi
 ln -s ~/.dotfiles/kitty ~/.config/kitty
 ln -s ~/.dotfiles/hypr ~/.config/hypr
 ln -s ~/.dotfiles/waybar ~/.config/waybar
+
+ln -s ~/.dotfiles/wofi-power-menu.toml ~/.config/wofi-power-menu.toml
+ln -s ~/.dotfiles/wallpapers ~/wallpapers
