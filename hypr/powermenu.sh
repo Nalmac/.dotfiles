2aa77daa5c78f5e051384fa277da7edb1e5ba610
= <=>
2026-01-09 15:36:03 +0100

    Power menu and app launcher

--- hypr/powermenu.sh
+++ hypr/powermenu.sh
@@ -1,35 +1,33 @@
 #!/bin/bash
 
 # Vérifier si wofi est installé
-if ! command -v wofi &> /dev/null; then
-    echo "wofi n'est pas installé. Veuillez l'installer d'abord."
-    exit 1
+if ! command -v wofi &>/dev/null; then
+  echo "wofi n'est pas installé. Veuillez l'installer d'abord."
+  exit 1
 fi
 
 # Options du menu
-options=("󰐥 Éteindre" "󰍃 Déconnexion" " Verrouillage" "󰜉 Redémarrer")
+options=("󰐥" "󰍃" "" "󰜉")
 
 # Afficher le menu avec wofi
-choice=$(printf '%s\n' "${options[@]}" | wofi -n --dmenu --cache-file /dev/null -p "Action désirée")
+choice=$(printf '%s\n' "${options[@]}" | wofi -c ~/.config/wofi/power-menu -s ~/.config/wofi/style-power.css)
 text_choice=$(echo "$choice" | cut -c 2- | xargs)
 
-
 case $choice in
-    "󰐥 Éteindre")
-        systemctl poweroff
-        ;;
-    "󰍃 Déconnexion")
-	    loginctl terminate-user $USER
-        ;;
-    " Verrouillage")
-       
-	gtklock
-        ;;
-    "󰜉 Redémarrer")
-        systemctl reboot
-        ;;
-    *)
-        echo "Option invalide"
-        ;;
-esac
+"󰐥")
+  systemctl poweroff
+  ;;
+"󰍃")
+  loginctl terminate-user $USER
+  ;;
+"")
 
+  gtklock
+  ;;
+"󰜉")
+  systemctl reboot
+  ;;
+*)
+  echo "Option invalide"
+  ;;
+esac
